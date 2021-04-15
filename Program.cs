7e3d44f8358bb1ef04e7bf799842c374c07173ec
heroofgotham <annamikhnenko9@gmail.com>
2021-04-15 12:57:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP_4laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string randomString = "tokio drift vrooom vrooom vrmoomovm";
+            char[] array = randomString.ToCharArray();
+            Strings CS1 = new Strings();
+            Strings CS2 = new Strings(array);
+            Strings CS3 = new Strings(CS2);
+            Console.WriteLine("\nStrings before changes:");
+            CS1.PrinterText();
+            CS2.PrinterText();
+            CS3.PrinterText();
+            CS1 = CS2 + CS3;
+            CS3 = CS3 - 'o';
+            Console.WriteLine("\nStrings after changes:");
+            CS1.PrinterText();
+            CS2.PrinterText();
+            CS3.PrinterText();
+
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_4laba
+{
+    class Strings
+    {
+        private char[] array;
+        private int length;
+
+        public Strings()
+        {
+
+        }
+
+        public Strings (char[] temp)
+        {
+            array = temp;
+            length = temp.Length;
+        }
+
+        public Strings(Strings Copy)
+        {
+            length = Copy.length;
+            array = Copy.array;
+        }
+
+        public char[] Array
+        {
+            get
+            {
+                return array;
+            }
+            set
+            {
+                array = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+            }
+        }
+
+        public static Strings operator +(Strings string1, Strings string2)
+        {
+            Strings CS = new Strings();
+            CS.Length = string1.Length + string2.Length;
+            char[] temp = new char[string1.Length + string2.Length];
+            for (int i = 0; i < string1.Length; i++)
+            {
+                temp[i] = string1.Array[i];
+            }
+            for (int i = string1.Length; i < string1.Length + string2.Length; i++)
+            {
+                temp[i] = string2.Array[i - string1.Length];
+            }
+            CS.Array = temp;
+            return CS;
+        }
+
+        public static Strings operator -(Strings string1, char Char)
+        {
+            int symbol = 0;
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if(string1.Array[i] != Char)
+                {
+                    symbol++;
+                }
+            }
+            char[] temp = new char[symbol];
+            symbol = 0;
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (string1.Array[i] != Char)
+                {
+                    temp[symbol] = string1.Array[i];
+                    symbol++;
+                }
+            }
+            Strings CS = new Strings(temp);
+            return CS;
+        }
+
+        public void PrinterText()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
